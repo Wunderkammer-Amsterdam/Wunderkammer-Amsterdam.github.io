efda30169c415e648fd4ef4fee4c0a27336bc804
Bas Kamer <bas@bushbaby.nl>
2020-10-02 14:57:15 +0200

    allow occupancy wit time

--- public/occupancy.php
+++ public/occupancy.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laminas\Json\Json;
+
 require_once('vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -13,12 +15,10 @@ if (! file_exists($storagePath)) {
     file_put_contents($storagePath, '[]');
 }
 
-$dataPoints = json_decode(file_get_contents($storagePath));
+$dataPoints = Json::decode(file_get_contents($storagePath));
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-
-    $date = new DateTimeImmutable('now', new DateTimeZone('utc'));
+    $data = Json::decode(file_get_contents('php://input'));
 
     if (isset($data['occupancy']) and is_numeric($data['occupancy'])) {
         $occupancy = (int) $data['occupancy'];
@@ -26,20 +26,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $occupancy = 'error';
     }
 
-    $responsePayload['on'] = $date->format(DATE_ATOM);
     $responsePayload['occupancy'] = $occupancy;
+
+    if (isset($data['on']) and $date = DateTimeImmutable::createFromFormat(DATE_ATOM, $data['on'])) {
+        $responsePayload['on'] = $date->format(DATE_ATOM);
+    } else {
+        $date = new DateTimeImmutable('now', new DateTimeZone('utc'));
+        $responsePayload['on'] = $date->format(DATE_ATOM);
+    }
+
 
     if ($responsePayload['occupancy'] !== 'error') {
         $dataPoints = array_slice($dataPoints, 0, 4 * 24 * 7 * 4);
 
         $dataPoints[] = $responsePayload;
-        file_put_contents($storagePath, json_encode($dataPoints));
+        file_put_contents($storagePath, Json::encode($dataPoints));
     }
 
 
     header('Content-Type: application/json');
 
-    print json_encode($responsePayload);
+    print Json::encode($responsePayload);
 
     exit();
 }
@@ -47,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Content-Type: application/json');
 
-    print json_encode($dataPoints);
+    print Json::encode($dataPoints);
 
     exit();
 }
