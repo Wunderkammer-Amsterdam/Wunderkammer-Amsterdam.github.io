e10e7823c75fa27ac5a811153847b7310c57c07c
Bas Kamer <bas@bushbaby.nl>
2020-10-07 12:40:24 +0200

    refresh 'bezetting' every 30s

--- app/pods/occupancy/route.js
+++ app/pods/occupancy/route.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import { inject as service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
 
 export default class BezettingRoute extends Route {
   @service headData;
@@ -11,13 +12,23 @@ export default class BezettingRoute extends Route {
 
   model() {
     return fetch('/occupancy.php').then((response) => {
+      this.refreshTask.perform();
+
       if (response.ok) {
         return response.json();
       } else {
         let error = new Error(response.statusText);
         error.response = response;
+
         throw error;
       }
     });
   }
+
+  @task(function* () {
+    yield timeout(30000);
+
+    yield this.refresh();
+  })
+  refreshTask;
 }
